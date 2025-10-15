a03cafecf8a37f71baca2d767f02456ba23c32d7
Kyle Shillingstad <kyleshillingstad@gmail.com>
2025-10-15 09:47:43 -0500

    test

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -5,7 +5,7 @@
   services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.nvidia-container-toolkit.enable = true;
-  hardware.opengl.enable32Bit = true;
+  hardware.opengl.driSupport32Bit = true;
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = false;
